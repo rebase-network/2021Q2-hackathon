30d92cb0e80b3600463efbb8d90059c8c6fae567
AprocySanae <axdiaoqi220@gmail.com>
2021-05-15 11:15:39 +0800

    update

--- projects/dAes/dAes/contracts/ERC721Ex.sol
+++ projects/dAes/dAes/contracts/ERC721Ex.sol
@@ -1,3 +1,5 @@
+pragma solidity ^0.8.0
+
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
