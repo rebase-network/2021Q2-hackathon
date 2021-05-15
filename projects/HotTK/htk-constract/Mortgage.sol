494f16751e922068deb8163bf8f08a8b5028861c
installi <82922235+installi@users.noreply.github.com>
2021-05-15 16:34:01 +0800

    Update Mortgage.sol

--- projects/HotTK/htk-constract/Mortgage.sol
+++ projects/HotTK/htk-constract/Mortgage.sol
@@ -9,7 +9,7 @@ import "@openzeppelin/contracts/math/SafeMath.sol";
  * @title The Three Kingdoms Mortgage
  *
  * @author Bit Lighthouse. Ace
- * AT: 2021-3-28 | VERSION: v1.0.2
+ * AT: 2021-5-15 | VERSION: v1.0.2
  */
 contract Mortgage is BaseMortage {
     
