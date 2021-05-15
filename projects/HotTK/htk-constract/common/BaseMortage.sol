c46829c6d7d31cdccc4abde237cc3492e442df83
installi <82922235+installi@users.noreply.github.com>
2021-05-15 16:36:35 +0800

    Update BaseMortage.sol

--- projects/HotTK/htk-constract/common/BaseMortage.sol
+++ projects/HotTK/htk-constract/common/BaseMortage.sol
@@ -11,7 +11,7 @@ import "@openzeppelin/contracts/math/SafeMath.sol";
  * @title The Three Kingdoms Base Mortgage
  *
  * @author Bit Lighthouse. Ace
- * AT: 2021-3-28 | VERSION: v1.0.2
+ * AT: 2021-5-14 | VERSION: v1.0.2
  */
 
 abstract contract BaseMortage is Common, Utils {
