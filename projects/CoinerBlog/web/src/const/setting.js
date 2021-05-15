0a5fca7d28f674221c1f6f66c4a23f4d2c983a6a
33357 <43600005+33357@users.noreply.github.com>
2021-05-15 11:20:07 +0800

    0.0.1

---
+++ projects/CoinerBlog/web/src/const/setting.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: 33357
+ * @Date: 2021-05-15 11:11:49
+ * @LastEditTime: 2021-05-15 11:12:50
+ * @LastEditors: 33357
+ */
+
+export const NETWORK_ID = 256;
+
+export const NETWORK_NAME = "HECO测试网";
---
+++ projects/CoinerBlog/web/src/web3/web3Provider.js
@@ -0,0 +1,52 @@
+/*
+ * @Author: 33357
+ * @Date: 2021-05-15 11:02:36
+ * @LastEditTime: 2021-05-15 11:14:37
+ * @LastEditors: 33357
+ */
+import detectEthereumProvider from "@metamask/detect-provider";
+import Web3 from "web3";
+import Router from "./contracts/router";
+import PointPool from "./contracts/pointPool";
+import Setting from "../const/setting";
+
+export class web3Provider {
+  async getWeb3() {
+    const provider = await detectEthereumProvider();
+    if (provider) {
+      if (provider !== window.ethereum) {
+        throw new Error("请使用支持web3的浏览器打开!");
+      } else {
+        const ethereum = window.ethereum;
+        if (ethereum) {
+          ethereum.on("accountsChanged", (accounts) => {
+            window.location.reload();
+          });
+          ethereum.on("chainChanged", (chainId) => {
+            window.location.reload();
+          });
+          try {
+            await ethereum.request({ method: "eth_requestAccounts" });
+          } catch (error) {
+            throw new Error("拒绝连接！");
+          }
+        }
+        this.web3 = new Web3(provider);
+        this.router = new this.web3.eth.Contract(Router.abi, Router.address);
+        this.pointPool = new this.web3.eth.Contract(
+          PointPool.abi,
+          PointPool.address
+        );
+        const res = await this.web3.eth.net.getId();
+        if (res !== Setting.NETWORK_ID) {
+          throw new Error("不是" + Setting.NETWORK_NAME);
+        }
+        const accounts = await this.web3.eth.getAccounts();
+        this.walletAddress = accounts[0];
+        return { walletAddress: this.walletAddress };
+      }
+    } else {
+      throw new Error("请使用支持web3的浏览器打开");
+    }
+  }
+}
